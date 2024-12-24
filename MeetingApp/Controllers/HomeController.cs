7b8ffdaa1503776b1905acd971fd45501b100443
Alper Can Tufan <alpcan9868@gmail.com>
2024-12-24 15:27:51 +0300

    3. Hafta bitti

---
+++ MeetingApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class HomeController:Controller
+    {
+        // localhost/
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+
+            ViewData["Selamlama"] = saat > 12? "İyi Günler":"Günaydın";
+            ViewData["UserName"] = "Çınar";
+
+            var meetingInfo = new MeetingInfo()
+            {
+                Id=1,
+                Location="İstanbul , Abc Kongre Merkezi",
+                Date = new DateTime(2024,01,20,20,0,0),
+                NumberOfPeople = 100
+            };
+
+            return View(meetingInfo);
+        }
+    }
+}
